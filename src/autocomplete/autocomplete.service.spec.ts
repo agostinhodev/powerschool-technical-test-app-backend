ef720df4f539e71e704c5f5b71123e0ea31ee8a1
agostinhodev <agostinho.developer@gmail.com>
2023-04-13 19:43:43 -0300

    test: :white_check_mark: added more tests

--- src/autocomplete/autocomplete.service.spec.ts
+++ src/autocomplete/autocomplete.service.spec.ts
@@ -16,16 +16,44 @@ describe('AutocompleteService', () => {
     expect(service).toBeDefined();
   });
 
+  //My tests
   it('should return an array of countries', () => {
     const countries = service.findAll();
     expect(countries).toBeDefined();
     expect(countries.length).toBeGreaterThan(0);
   });
 
-  it('should return filtered countries when a search term is provided', () => {
+  it('should return all the countries in the world', () => {
+    const countries = service.findAll();
+    expect(countries).toBeDefined();
+    expect(countries.length).toStrictEqual(243); //Total countries in the world
+  });
+
+  it('should return Brazil', () => {
     const countries = service.findAll('BRA');
     expect(countries).toBeDefined();
     expect(countries.length).toBeGreaterThan(0);
     expect(countries[0].name).toEqual('Brazil');
+  });
+
+  it('should return United States', () => {
+    const countries = service.findAll('United Sta');
+    expect(countries).toBeDefined();
+    expect(countries.length).toBeGreaterThan(0);
+    expect(countries[0].name).toEqual('United States');
+  });
+
+  it('should return the two Koreas', () => {
+    const countries = service.findAll('korea');
+    expect(countries).toBeDefined();
+    expect(countries.length).toStrictEqual(2);
+    expect(countries[0].name).toEqual("Korea, Democratic People'S Republic of");
+    expect(countries[1].name).toEqual('Korea, Republic of');
+  });
+
+  it('should return an empty array when search for Zorgonikovia', () => {
+    const countries = service.findAll('Zorgonikovia');
+    expect(countries).toBeDefined();
+    expect(countries.length).toEqual(0);
   });
 });
