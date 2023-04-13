030383681e15ccdb308bbac042f7ffbc35bd7bbe
agostinhodev <agostinho.developer@gmail.com>
2023-04-13 17:54:43 -0300

    chore: :fire: removed some unecessary files at this moment

--- src/autocomplete/dto/update-autocomplete.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAutocompleteDto } from './create-autocomplete.dto';
-
-export class UpdateAutocompleteDto extends PartialType(CreateAutocompleteDto) {}
