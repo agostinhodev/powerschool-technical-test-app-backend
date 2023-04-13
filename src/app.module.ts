3b68ddabb831baf80f85ad6fa6bff9b54d747664
agostinhodev <agostinho.developer@gmail.com>
2023-04-13 15:00:18 -0300

    chore: :fire: removed / path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AutocompleteModule } from './autocomplete/autocomplete.module';
 
 @Module({
   imports: [AutocompleteModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
