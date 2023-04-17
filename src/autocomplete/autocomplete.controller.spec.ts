f9822fa2cd5fbdf482e1f3277130d7aaac73c8c1
agostinhodev <agostinho.developer@gmail.com>
2023-04-17 14:39:47 -0300

    test: :white_check_mark: updated tests

--- src/autocomplete/autocomplete.controller.spec.ts
+++ src/autocomplete/autocomplete.controller.spec.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AutocompleteController } from './autocomplete.controller';
 import { AutocompleteService } from './autocomplete.service';
+import { AutocompleteQueryParams } from './dto/autocomplete-quer-params.dto';
 
 describe('AutocompleteController', () => {
   let controller: AutocompleteController;
@@ -23,40 +24,66 @@ describe('AutocompleteController', () => {
 
   // My tests
   describe('findAll', () => {
-    it('should return a list of countries when given a valid name', () => {
+    it('should return a list of countries when given a valid name', async () => {
       const result = [{ name: 'Brazil', code: 'BR' }];
       jest.spyOn(service, 'findAll').mockReturnValue(result);
 
-      expect(controller.findAll('BRA')).toEqual(result);
+      const queryParams = new AutocompleteQueryParams();
+      queryParams.name = 'BRA';
+      const response = await controller.findAll(queryParams);
+
+      expect(response).toEqual(result);
       expect(service.findAll).toHaveBeenCalledWith('BRA');
     });
 
-    it('should return a 404 error when no countries are found', () => {
+    it('should return a 404 error when no countries are found', async () => {
       jest.spyOn(service, 'findAll').mockReturnValue([]);
 
-      expect(() => controller.findAll('invalid')).toThrowError(
-        new HttpException('No results found', HttpStatus.NOT_FOUND),
+      const queryParams = new AutocompleteQueryParams();
+      queryParams.name = 'ZAZAZAZAZAZZA';
+
+      await expect(controller.findAll(queryParams)).rejects.toThrow(
+        HttpException,
       );
-      expect(service.findAll).toHaveBeenCalledWith('invalid');
+      await expect(controller.findAll(queryParams)).rejects.toThrowError(
+        'No results found',
+      );
+      await expect(controller.findAll(queryParams)).rejects.toHaveProperty(
+        'status',
+        HttpStatus.NOT_FOUND,
+      );
+
+      expect(service.findAll).toHaveBeenCalledWith('ZAZAZAZAZAZZA');
     });
 
-    it('should return a list of all countries when no name is given', () => {
+    it('should return a list of all countries when no name is given', async () => {
       const result = [
         { name: 'Brazil', code: 'BR' },
         { name: 'Canada', code: 'CA' },
       ];
       jest.spyOn(service, 'findAll').mockReturnValue(result);
 
-      expect(controller.findAll()).toEqual(result);
+      const queryParams = new AutocompleteQueryParams();
+      await expect(controller.findAll(queryParams)).resolves.toEqual(result);
+
       expect(service.findAll).toHaveBeenCalledWith(undefined);
     });
 
-    it('should return a 500 error when an error occurs in the service', () => {
+    it('should return a 500 error when an error occurs in the service', async () => {
       jest.spyOn(service, 'findAll').mockImplementation(() => {
-        throw new Error('Internal server error');
+        throw new HttpException(
+          'Internal server error',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
       });
 
-      expect(() => controller.findAll('invalid')).toThrowError(
+      const queryParams = new AutocompleteQueryParams();
+      queryParams.name = 'invalid';
+
+      await expect(controller.findAll(queryParams)).rejects.toThrow(
+        HttpException,
+      );
+      await expect(controller.findAll(queryParams)).rejects.toThrowError(
         new HttpException(
           'Internal server error',
           HttpStatus.INTERNAL_SERVER_ERROR,
